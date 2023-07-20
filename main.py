427958531c65e0a31aceeedc605179a6b543bb78
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-07-20 10:20:06 +0300

    creating a database and a table inside it

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    #text = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -11,7 +22,6 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 
 if __name__ == "__main__":
