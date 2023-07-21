67fa2a16b6166293dbd481077257faed9efb1d99
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-07-21 09:42:46 +0300

    added method post and get

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -26,9 +26,23 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Получилась ошибка'
+
+    else:
+        return render_template('create.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
