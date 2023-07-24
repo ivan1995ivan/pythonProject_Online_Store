9513d8b4de3519843042f8f465e1ca54d6e8d144
ivan1995ivan <111653497+ivan1995ivan@users.noreply.github.com>
2023-07-24 10:07:48 +0300

    added method retrieve records from databasepost and get

--- main.py
+++ main.py
@@ -14,11 +14,15 @@ class Item(db.Model):
     isActive = db.Column(db.Boolean, default=True)
     #text = db.Column(db.Text, nullable=False)
 
+    def __repr__(self):
+        return f"Запись: {self.title}"
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html',data=items)
 
 
 @app.route('/about')
